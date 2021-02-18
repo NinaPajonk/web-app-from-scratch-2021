074605a8d0bc281fc27e6852c7eb06e0f79aae19
NinaPajonk <50745190+NinaPajonk@users.noreply.github.com>
2021-02-18 22:58:46 +0100

    overview page top 5 + images

--- scripts/modules/locations.js
+++ scripts/modules/locations.js
@@ -1,36 +1,26 @@
 export const locations = [{
     "id" : 1,
-    "cityName" : "IJmuiderslag, Ijmuiden",
-    "long": 52.4573,   
-    "lat": 4.6111,
+    "name" : "IJmuiden",
     "image": "https://www.powerkiteschool.nl/wp-content/uploads/2016/09/kite-surfen10-2.jpg"
 },
 {
     "id" : 2,
-    "cityName" : "Noorderstrand, Scheveningen",
-    "long": 52.1046, 
-    "lat": 4.2756,
+    "name" : "Scheveningen",
     "image":"https://kitesurfschool.nl/wp-content/uploads/2020/12/ZWARTE-PAD-FOTOS.png"
 },
 {
     "id" : 3,
-    "cityName" : "Noordwijk strand, Noordwijk",
-    "long": 52.2411, 
-    "lat": 4.4462 ,
+    "name" : "Noordwijk",
     "image": "https://media.insiders.nl/nwm/files/image/beach-break-exterieur-2019_98221694.jpeg"
 },
 {
     "id" : 4,
-    "cityName" : " Ameland Noordzee, Ameland",
-    "long": 53.0833, 
-    "lat":4.8 ,
+    "name" : "Ameland",
     "image": "https://i.pinimg.com/originals/4a/91/30/4a9130b89feaf3d2638f47cc4bab64be.jpg"
 },
 {
     "id" : 5,
-    "cityName": " Terschelling Groene strand, Terschelling",
-    "long": 53.3591, 
-    "lat": 5.2148 ,
+    "name": "West-terschelling",
     "image": "https://thumbs.werkaandemuur.nl/1/ab5d34484c5aea09fd0f065fe8de6230/817x600/thumbnail/fit.jpg"
 }
 ]
--- scripts/modules/router.js
+++ scripts/modules/router.js
@@ -1,13 +1,9 @@
-import {
-    locations
-}
-from './locations.js'
 
-// router 
-function router() {
-    routie('#hallo', function () {
-        console.log('test')
-    });
-};
+// // router 
+// function router() {
+//     routie('#hallo', function () {
+//         console.log('test')
+//     });
+// };
 
-router()
+// router()
